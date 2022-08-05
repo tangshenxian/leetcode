06fc72e31a1884598d5ad270ccb9d96a2be233cd
tangshenxian <1125584876@qq.com>
2022-08-05 15:52:58 +0800

    add a row to binary tree

---
+++ [623][add a row to binary tree]/src/solution/Solution.java
@@ -0,0 +1,40 @@
+package solution;
+
+/**
+ * @author: shenxian
+ * @date: 2022/8/5 15:44
+ */
+public class Solution {
+
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (root == null) {
+            return null;
+        }
+        if (depth == 1) {
+            return new TreeNode(val, root, null);
+        }
+        if (depth == 2) {
+            root.left = new TreeNode(val, root.left, null);
+            root.right = new TreeNode(val, null, root.right);
+        }
+        if (depth > 2) {
+            root.left = addOneRow(root.left, val, depth - 1);
+            root.right = addOneRow(root.right, val, depth - 1);
+        }
+        return root;
+    }
+
+}
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
