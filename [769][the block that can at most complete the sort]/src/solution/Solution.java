9066177ac24b01dc89039eab6d0aee7fd07829b1
tangshenxian <1125584876@qq.com>
2022-10-13 10:08:31 +0800

    the block that can at most complete the sort

---
+++ [769][the block that can at most complete the sort]/src/solution/Solution.java
@@ -0,0 +1,22 @@
+package solution;
+
+/**
+ * 769. 最多能完成排序的块
+ *
+ * @author: shenxian
+ * @date: 2022/10/13 9:26
+ */
+public class Solution {
+
+    public int maxChunksToSorted(int[] arr) {
+        int max = 0, res = 0;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+            if (max == i) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+}
